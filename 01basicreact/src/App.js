ce322a9d6ab52cd1ad360808f6f85ace0c780550
saphaltaneupane <neupanesaphalta@gmail.com>
2025-06-17 19:35:05 +0545

    oky

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,11 @@
+// App.js
 import React from 'react';
-import Chai from "./chai";
+import Chai from './chai'; // ✅ Correct import (case-sensitive, relative path)
+
 function App() {
-  //  return <h1>Hello World | Saphalta Neupane  bulky version this is  vite is not can be done by vite </h1>;
-  return <Chai />;
+  // return <h1>Hello World | Saphalta Neupane  bulky version this is  vite is not can be done by vite </h1>;
+  return <Chai />; // ✅ Now correctly uses the imported Chai component
 }
 
 export default App;
+
