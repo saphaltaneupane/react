eff63f485f5b23b0510d7ba8861f3de214c2d84c
saphaltaneupane <neupanesaphalta@gmail.com>
2025-07-28 07:22:24 +0545

    doing

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -4,7 +4,8 @@ import Chai from './chai'; // ✅ Correct import (case-sensitive, relative path)
 
 function App() {
   // return <h1>Hello World | Saphalta Neupane  bulky version this is  vite is not can be done by vite </h1>;
-  return <Chai />; // ✅ Now correctly uses the imported Chai component
+   return <h1>Hello World | Saphalta Neupane  can be created by two types create react app and create vitae app react app is bulky process vite is light weighted mostly used vite react have multiple librabry </h1>; // ✅ Simplified return statement
+  // return <Chai />; // ✅ Now correctly uses the imported Chai component
 }
 
 export default App;
