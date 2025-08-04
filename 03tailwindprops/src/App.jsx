33cc80134b1c5574dd17c6c3dc3ccaebcfe5af22
saphaltaneupane <neupanesaphalta@gmail.com>
2025-08-04 19:37:03 +0545

    doing

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+      
+    </>
+  )
+}
+
+export default App
