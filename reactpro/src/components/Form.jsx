81599c437160ea487e09df354baa217e43817c21
saphaltaneupane <neupanesaphalta@gmail.com>
2025-09-03 21:01:42 +0545

    okay

--- reactpro/src/components/Form.jsx
+++ reactpro/src/components/Form.jsx
@@ -1,14 +1,24 @@
 import {useState} from "react";
 
 export default function Form(){
-    const [name,setName]=useState("")
-   
+    const [name,setName]=useState({firstName:"", lastName:""});
+
+   function handleSubmit(e){
+    e.preventDefault();
+    console.log(name.firstName);
+    console.log(name.lastName);
+   }
     return <div>
+        {name.firstName} {name.lastName}
         <form >
             <input  
             // onChange={function demo(e){return handleChange(e);}
-            onChange={(e)=>setName(e.target.value)}
-         type="text"  value={name}/>
+            onChange={(e)=>setName({...name, firstName:e.target.value})}
+         type="text"  value={name.firstName}/>
+         <input
+            onChange={(e)=>setName({...name,  lastName:e.target.value})}
+         type="text"  value={name.lastName}/>
+         <button onClick={(e)=>handleSubmit(e)}>Add</button>
         </form>
     </div>
-}
+} 
